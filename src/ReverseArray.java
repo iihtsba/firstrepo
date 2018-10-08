547c24729672fdfff1dbca3dc574b546cfbeb1b6
unknown <vskreddy652@yahoo.com>
2018-10-08 22:31:55 +0530

    First Commit

---
+++ src/ReverseArray.java
@@ -0,0 +1,21 @@
+class Email 
+{ 
+public void sendEmail(String subject, String message)
+{ 
+	System.out.println("Mail sending successfull");
+} 
+} 
+
+
+public class ReverseArray  
+{ 
+private Email email = new Email(); 
+public void greetFriend()
+{ 
+	email.sendEmail("Hello", "Hello my friend "); 
+} 
+
+public static void main(String[] args) {
+	new ReverseArray().greetFriend();
+}
+} 
